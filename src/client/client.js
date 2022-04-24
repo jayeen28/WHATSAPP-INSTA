a8cae6d0eb276d28fd7b80a647e14bde3333d0e3
Jayeen28 <jayeen6783@gmail.com>
2022-04-24 18:11:56 +0600

    client updated

--- src/client/client.js
+++ src/client/client.js
@@ -1,11 +1,7 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const client = new Client(
     {
-        authStrategy: new LocalAuth({ dataPath: './' }),
-        puppeteer: {
-            headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
-        }
+        authStrategy: new LocalAuth({ dataPath: './' })
     }
 );
 
